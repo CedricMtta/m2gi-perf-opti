2e173d6078d7b0b784ec67918d62eced26bda75b
CedricMtta <cedricmtta@gmail.com>
2025-01-08 20:42:06 +0100

    :tada: Project initialization

---
+++ src/main/java/com/cedricmtta/perf_opti/PerfOptiApplication.java
@@ -0,0 +1,13 @@
+package com.cedricmtta.perf_opti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfOptiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfOptiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cedricmtta/perf_opti/PerfOptiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cedricmtta.perf_opti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfOptiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
